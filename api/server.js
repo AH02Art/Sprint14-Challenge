ef5ff40d9bbe1c60cc2375e061f9f4864e24bc68
Alex Handley <smithvillescholarsah@gmail.com>
2024-08-05 17:05:06 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.use("*", function(request, response) {
+    response.json({ 
+        project: "Sprint14-Challenge",
+        developer: "Alex Handley" 
+    });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+require("dotenv").config();
+
+const server = require('./api/server.js');
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, function() {
+  console.log(`\n*** project running on: http://localhost:${PORT}/ ***\n`);
+});
