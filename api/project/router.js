a3b89e92c6626a17bc4b7c238147df9dfe273457
Alex Handley <smithvillescholarsah@gmail.com>
2024-08-09 19:24:35 -0500

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
-// const Project = require("./model.js");
+const Project = require("./model.js");
 
-router.get("/", function(request, response) {
-    response.send("GET projects here");
+router.get("/", function(request, response, next) {
+    Project.getProjects()
+    .then((data) => {
+        const stuff = data.map((project) => {
+            return {
+                ...project,
+                project_completed: project.project_completed === 1
+            };
+        });
+        response.json(stuff);
+    })
+    .catch(next);
 });
 
-router.post("/", function(request, response) {
-    response.send("POST projects here");
+router.post("/", function(request, response, next) {
+    const project = request.body;
+    console.log("req.body ===> ", project);
+
+    Project.postProject(project)
+        .then((p) => {
+            console.log("did something happen?? ===> ", p);
+            response.status(201).json(p);
+        })
+        .catch(next);
 });
 
 router.use(function(error, request, response, next) {
